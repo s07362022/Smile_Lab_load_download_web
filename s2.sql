2ff3882aec4ff3e283d9eaded12b1895c9cd1477
Li_Harden <68153902+s07362022@users.noreply.github.com>
2024-07-18 21:56:37 +0800

    Add files via upload

---
+++ s2.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE harden_web_micor;
+
+USE harden_web_micor;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    `key` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    path VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE classes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    folder VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE upload_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    class VARCHAR(50),
+    file_count INT,
+    filenames JSON,
+    timestamp DATETIME
+);
+
+CREATE TABLE download_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    `key` VARCHAR(255),
+    filename VARCHAR(255),
+    timestamp DATETIME
+);
+
+select * from download_logs;
+select * from upload_logs;
+select * from files;
